e9fae372d0bbc55ab2d44bb821427aa122d342b9
Natan Derick <114193508+NatanXT@users.noreply.github.com>
2025-02-07 17:04:46 -0300

    Criação
    Criado a model, do projeto, juntamento com o caminho das URL's, classe de order_urls e de usuario

--- Loja_Django_React/backend/backend/settings.py
+++ Loja_Django_React/backend/backend/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 import os
+from datetime import timedelta
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
